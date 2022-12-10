a0d2553b4122b6836a107064faa7f25c8e8d3e0b
rajhah <109645238+rajhah@users.noreply.github.com>
2022-12-10 13:52:18 -0600

    Added main.js and index.html. Implemented basic functionality into js file and linked it to index.html

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,8 @@
+var Sent1 = "This is the beginning of the string,"; // Assigning a string value to the variable Sent1
+var Sent2 = " and this is the end of the string.";  // Assigning a string value to the variable Sent2
+
+var concatSent = (Sent1 + Sent2);       // Concatenating the two strings Sent1 and Sent2
+var daysWithoutAccident = (50 - 50);    // Here is a line of code with an expression
+
+window.alert("This page has gone " + daysWithoutAccident + " days without an accident."); // Using window.alert to display a string and a variable
+document.write(concatSent);     // Using the document.write alert to write out our concatenated string from earlier.
